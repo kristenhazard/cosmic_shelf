60001805840be7f1a4f28b94402b55c3e3816ef7
Suntoucher Support <support@suntouchersoftware.com>
2015-03-20 14:10:03 -0700

    moved all javascript calls in kristen.js into separate functions. Calling functions on window.load an a nested window.resize

--- app/assets/javascripts/kristen.js
+++ app/assets/javascripts/kristen.js
@@ -1,51 +1,58 @@
-$(document).ready( function() {
-
-/* Variables for cosmic bookshelf */
-
-var shelf = $('.shelf-container');
-var bookList = $('.shelf-container ul');
-var book = $('.shelf-container li');
-var containerWidth = book.closest(shelf).width() * 0.2;
-
-
-/* size books according to book count in relation to shelf width */
-
-$(bookList).each(function() {
-	if ($(this).has('li')) {
-		var bookCount = $(this).children().length;
-		if (bookCount <= 2) {
-			$(this).children().css('width', ($(this).closest('.shelf-container').width() * 0.5));
-		}
-		if (bookCount < 4) {
-			$(this).children().css('width', ($(this).closest('.shelf-container').width() * 0.25));
-		}
-		if (bookCount >= 4) {
-			$(this).children().css('width', ($(this).closest('.shelf-container').width() * 0.15));
-		}
-	}
-});
-
-/* This works in conjunction with the zoom-in css hover effect. It gives the element a high z-index whilst hovered */
-
-$(book).hover(function() {
-	$(this).css('z-index', '200');
-}, function() {
-	$(this).css('z-index', '10');
-});
-
-/* Scaling text in the book cover. */
-
-});
-
 $(window).load(function() {
-var book = $('.shelf-container li');
-$(book).each(function() {
-	var bookHeight = $(this).height();
-	var titleSize = bookHeight * 0.15;
-	var subtitleSize = bookHeight * 0.075;
-	var authorSize = bookHeight * 0.05;
-	$(this).find('h1').css('font-size', titleSize);
-	$(this).find('h4').css('font-size', subtitleSize);
-	$(this).find('h6').css('font-size', authorSize);
-});
+
+	/* Variables for cosmic bookshelf */
+	var shelf = $('.shelf-container');
+	var bookList = $('.shelf-container ul');
+	var book = $('.shelf-container li');
+	var containerWidth = book.closest(shelf).width() * 0.2;
+
+	bookSizr();
+	textSizr();
+	setZoomIndex();
+
+	$(window).resize(function() {
+		bookSizr();
+		textSizr();
+		setZoomIndex();
+	});
+
+	/* This works in conjunction with the zoom-in css hover effect. It gives the element a high z-index whilst hovered */
+	function setZoomIndex() {
+		$(book).hover(function() {
+			$(this).css('z-index', '200');
+		}, function() {
+			$(this).css('z-index', '10');
+		});
+	}
+
+	/* size books according to book count in relation to shelf width */
+	function bookSizr() {
+		$(bookList).each(function() {
+			if ($(this).has('li')) {
+				var bookCount = $(this).children().length;
+				if (bookCount <= 2) {
+					$(this).children().css('width', ($(this).closest(shelf).width() * 0.5));
+				}
+				if (bookCount < 4) {
+					$(this).children().css('width', ($(this).closest(shelf).width() * 0.25));
+				}
+				if (bookCount >= 4) {
+					$(this).children().css('width', ($(this).closest(shelf).width() * 0.15));
+				}
+			}
+		});
+	}
+
+	/* Scaling text in the book cover. */
+	function textSizr() {
+		$(book).each(function() {
+			var bookHeight = $(this).height();
+			var titleSize = bookHeight * 0.15;
+			var subtitleSize = bookHeight * 0.075;
+			var authorSize = bookHeight * 0.05;
+			$(this).find('h1').css('font-size', titleSize);
+			$(this).find('h4').css('font-size', subtitleSize);
+			$(this).find('h6').css('font-size', authorSize);
+		});
+	}
 });
